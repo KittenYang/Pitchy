e121af36356d932af9b5ad6588856644a2f80026
KittenYang <kittenyang@icloud.com>
2021-02-24 22:37:42 +0800

    update package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Pitchy"
+    name: "Pitchy",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "Pitchy",
+            targets: ["Pitchy"]),
+    ],
+    targets: [
+        .target(name: "Pitchy", path: "Source")
+    ]
 )
